e26777e41faa239cde89254506fc03be737dee78
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2024-10-17 16:58:21 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
             state = if (self ? rev) then "clean" else "dirty";
 
             # This vendorHash represents a derivative of all go.mod dependencies and needs to be adjusted with every change
-            vendorHash = "";
+            vendorHash = "sha256-pfnq3+5xm";
 
             src = ./.;
 
